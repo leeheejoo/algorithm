15c436edb868ba9dfc994c536bc68a37d15567e0
spdm1254 <spdm1254@gmail.com>
2019-05-26 16:01:24 +0900

    CyclicRotation, OddOccurrencesInArray 추가

---
+++ codility test/OddOccurrencesInArray.cpp
@@ -0,0 +1,79 @@
+/*
+
+A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+For example, in array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the elements at indexes 0 and 2 have value 9,
+the elements at indexes 1 and 3 have value 3,
+the elements at indexes 4 and 6 have value 9,
+the element at index 5 has value 7 and is unpaired.
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the function should return 7, as explained in the example above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an odd integer within the range [1..1,000,000];
+each element of array A is an integer within the range [1..1,000,000,000];
+all but one of the values in A occur an even number of times.
+
+
+https://app.codility.com/demo/results/training87JU4Z-F6V/
+
+*/
+
+
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+
+    map<int,int> valueCntMap;
+
+    for(int i=0; i < A.size(); i++){
+
+        int value = A[i];    
+        int count = valueCntMap[value];
+        valueCntMap[value] = count + 1;
+    }
+
+    map<int,int>::iterator itr = valueCntMap.begin();
+
+    for(; itr != valueCntMap.end(); itr++){
+
+        if(itr->second % 2 == 1)
+            return itr->first;
+
+    }
+
+    return 0;
+}
+
+
+int main() {
+
+    vector<int> A = {9,3,9,3,9,7,9};
+
+    int ret = solution(A);
+    printf("%d\n",ret);
+
+    getchar();
+
+    return 0;
+}
--- codility test/binaryGap.cpp
+++ codility test/binaryGap.cpp
@@ -16,6 +16,9 @@ Write an efficient algorithm for the following assumptions:
 
 N is an integer within the range [1..2,147,483,647].
 
+
+https://app.codility.com/demo/results/trainingGE6X7B-KE8/
+
 **/
 
 #include <string>
