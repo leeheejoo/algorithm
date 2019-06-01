94e51cf74933305e155737ac7dedb0b0d4dad768
spdm1254 <spdm1254@gmail.com>
2019-06-01 20:40:53 +0900

    CountFactors, MaxSliceSum,  MinPerimeterRectangle 추가

---
+++ codility test/CountFactors.cpp
@@ -0,0 +1,50 @@
+/*
+A positive integer D is a factor of a positive integer N if there exists an integer M such that N = D * M.
+
+For example, 6 is a factor of 24, because M = 4 satisfies the above condition (24 = 6 * 4).
+
+Write a function:
+
+int solution(int N);
+
+that, given a positive integer N, returns the number of its factors.
+
+For example, given N = 24, the function should return 8, because 24 has 8 factors, namely 1, 2, 3, 4, 6, 8, 12, 24. There are no other factors of 24.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..2,147,483,647].
+
+https://app.codility.com/demo/results/trainingZ7EGQW-UGV/
+*/
+
+#include <math.h> 
+
+using namespace std;
+
+int solution(int N) {
+
+    if(N == 1)
+        return 1;
+
+    int count = 2;
+    int squre = sqrt(N);
+    
+    for(int i=2; i<=squre; i++){
+
+        if(N%i == 0)
+            count += 2;
+    }
+
+    if(squre*squre == N)
+        count--;
+
+    return count;
+}
+
+int main() {
+
+    int ret = solution(1024);
+
+    return 0;
+}
